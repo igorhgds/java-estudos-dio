41a4af757710baf2ac3bdc39ac78fa8189bcdf40
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-25 07:56:30 -0300

    add repo spring-boot

---
+++ spring-boot/CadastroUsuario/src/main/java/dio/my_first_web_api/repository/UsuarioRepository.java
@@ -0,0 +1,42 @@
+package dio.my_first_web_api.repository;
+
+import dio.my_first_web_api.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+    public void save(Usuario usuario) {
+        if(usuario.getId()==null)
+            System.out.println("SAVE - Recebendo o usuario na camada de repositorio");
+        else
+            System.out.println("UPDATE - Recebendo o usuario na camada de repositorio");
+
+        System.out.println(usuario);
+    }
+
+    public void deleteById(Integer id) {
+        System.out.println(String.format("DELETE/id -  Recebendo o id: %d para excluir um usuario", id));
+        System.out.println(id);
+    }
+
+    public List<Usuario> findAll() {
+        System.out.println("LIST - Listando os usuarios do sistema");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("igor", "password"));
+        usuarios.add(new Usuario("lucas", "masterpassword"));
+        return usuarios;
+    }
+
+    public Usuario findById(Integer id) {
+        System.out.println(String.format("FIND/id -  Recebendo o id: %d para localizar um usuario", id));
+        return new Usuario("igor", "password");
+    }
+
+    public Usuario findByUserName(String username) {
+        System.out.println(String.format("FIND/username - Recebendo o username: %s para localizar um usuario", username));
+        return new Usuario("igor", "password");
+    }
+}
